290d629bb906b3b76a7082ba0f4b99d6169e8810
Idara Ekong <idaraekong2@gmail.com>
2023-04-05 15:52:39 +0000

    AirBnB 2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.197.82.23", "18.207.112.232"]
+env.hosts = ["100.27.0.105", "100.24.238.42"]
 
 
 def do_pack():
